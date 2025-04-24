518d745aa0ede2cfe7a2825e344fcebc6daaff17
harshit7922 <harshit.porwal78@gmail.com>
2025-04-24 22:52:56 +0530

    adding classname in dom at runtime

--- src/app/directives/highlighted.directive.ts
+++ src/app/directives/highlighted.directive.ts
@@ -10,10 +10,10 @@ export class HighlightedDirective {
     console.log('Highlighted directive initialized');
   }
 
-  @HostBinding('class.highlighted') // Correctly bind to the 'class.highlighted' property
+  @HostBinding('className') // Correctly bind to the 'class.highlighted' property
   get cssClass() {
     console.log('cssClass getter called');
   
-    return true; // Return true to apply the class
+    return "highlighted"; // Return true to apply the class
   }
 }
